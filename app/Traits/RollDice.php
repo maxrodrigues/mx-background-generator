8f22bf840f1d342a4db83e5bf87780b09c5fa831
Max Rodrigues <maxuel.rodrigues@gmail.com>
2022-07-25 13:23:12 -0300

    fix: chanfes in roll dice trait

--- app/Traits/RollDice.php
+++ app/Traits/RollDice.php
@@ -4,12 +4,39 @@ namespace App\Traits;
 
 trait RollDice
 {
-    public static function roll($num, $dice): array
+    /**
+     * Roll any dice and add ou remove some numbers
+     *
+     * @param  int $num
+     * @param  int $dice
+     * @param  int $plus
+     * @param  int $minus
+     * @return array
+     */
+    public static function roll(int $num, int $dice, int $plus = 0, int $minus = 0): array
     {
         $rolls = [];
         for ($i = 0; $i < $num; $i++) {
-            $rolls[] = rand(1, $dice);
+            $roll = rand(1, $dice);
+            if ($i === 0) {
+                $rolls['roll'] = $roll;
+            } else {
+                $rolls['roll'] .= " + " . $roll;
+            }
+
+            $rolls['total'] = isset($rolls['total']) ? $rolls['total'] + $roll : $roll;
         }
+
+        if ($plus > 0) {
+            $rolls['total'] += $plus;
+            $rolls['roll'] .= " + " . $plus;
+        }
+
+        if ($minus > 0) {
+            $rolls['total'] -= $minus;
+            $rolls['roll'] .= " - " . $minus;
+        }
+
         return $rolls;
     }
 }
